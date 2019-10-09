7222d8218ee4f8e9ed5169ceb4ee8f9c0b9994e3
Axel <axosar@gmail.com>
2019-10-09 16:10:59 -0500

    Adding Hooks

--- src/components/Introduccion.js
+++ src/components/Introduccion.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import './Introduccion.css';
-import logo from '../logo.svg';
 
-const Introduccion = () => {
+function Introduccion() {
+
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
     <div className="introduccion">
       <Link to="/"><h1>introducción</h1></Link>
@@ -15,6 +23,10 @@ const Introduccion = () => {
           </Link>
           <p>LABORATORIO</p>
         </div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
         <div className="item">sv</div>
         <div className="item">sv</div>
         <div className="item">sv</div>
