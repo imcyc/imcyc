6fe4af65df0a04f2cc2d491e3e12bbd56d0be397
Axel <axosar@gmail.com>
2019-10-11 12:49:52 -0500

    Cambios de plataforma

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import logo from '../logo.svg';
+import './Home.css';
 
 const Home = () => {
   return (
     <div className="App">
       <header className="App-header">
         <Link to="/introduccion"><img src={logo} className="App-logo" alt="logo" /></Link>
-        <p>Instituto Mexicano del Cemento y del Concreto A.C.</p>
+        <p className="imcyc">Instituto Mexicano del Cemento y del Concreto A.C.</p>
+        <p className="texto"><small>El IMCYC, A.C. es una asociación no lucrativa dedicada a la investigación, enseñanza y difusión de las técnicas de aplicación del cemento y del concreto. Su misión es promover la utilización óptima del cemento y del concreto para satisfacer las necesidades del mercado con calidad, productividad y oportunidad, contribuyendo a mejorar el desempeño profesional, el desarrollo y beneficio económico de la industria, así como de la sociedad.</small></p>
       </header>
     </div>
   );
--- src/components/Introduccion.js
+++ src/components/Introduccion.js
@@ -3,34 +3,70 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Prensa } from '../images/prensa.svg';
 import { ReactComponent as Planta } from '../images/plantas.svg';
 import { ReactComponent as Laboratorio } from '../images/laboratorio.svg';
+import { ReactComponent as Revolvedora } from '../images/revolvedora.svg';
+import { ReactComponent as Logoredcyc } from '../images/logo-redcyc.svg';
+import { ReactComponent as Logoautoconstruccion } from '../images/logo-autoconstruccion.svg';
+import { ReactComponent as Logodeacero } from '../images/logo-deacero.svg';
+import logo from '../logo.svg';
 import './Introduccion.css';
 
 function Introduccion() {
   return (
     <div className="introduccion">
+      <Link to="/introduccion"><img src={logo} className="App-logo" alt="logo" style={{width: '190px'}} /></Link>
       <Link to="/"><h1>APLICACIONES PARA LA CONSTRUCCIÓN</h1></Link>
       <h2>INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h2>
       <div className="container">
         <div className="item">
-          <Link to="/">
+          <a href="http://18.220.192.140:8080/" target="_blank">
+            <Revolvedora />
+          </a>
+          <h2><b>CERTIFICACIÓN</b><br/>REVOLVEDORA DE CONCRETO</h2>
+        </div>
+        <div className="item">
+          <a href="http://www.redcyc.com/plantasdeconcreto/" target="_blank">
             <Planta />
-          </Link>
+          </a>
           <h2><b>CERTIFICACIÓN</b><br/>PLANTAS DE CONCRETO</h2>
         </div>
         <div className="item">
-          <Link to="/">
+          <a href="http://18.216.111.243/" target="_blank">
             <Prensa />
-          </Link>
+          </a>
           <h2><b>CERTIFICACIÓN</b><br/>PRENSAS DE CONCRETO</h2>
         </div>
         <div className="item">
-          <Link to="/">
+          <a href="http://18.188.251.124/" target="_blank">
             <Laboratorio />
-          </Link>
+          </a>
           <h2><b>CERTIFICACIÓN</b><br/>LABORATORIO</h2>
         </div>
-        <div className="item">sv</div>
-        <div className="item">sv</div>
+      </div>
+      <div className="container">
+        <div className="item">
+          <a href="http://www.redcyc.com/" target="_blank">
+            <Logoredcyc />
+          </a>
+          <h2><b>PLATAFORMA</b><br/>REDCYC</h2>
+        </div>
+        <div className="item">
+          <a href="https://www.auto-construccion.com" target="_blank">
+            <Logoautoconstruccion />
+          </a>
+          <h2><b>PLATAFORMA</b><br/>AUTOCONSTRUCCIÓN</h2>
+        </div>
+        <div className="item">
+          <a href="http://imcyc.net/acero/" target="_blank">
+            <Logodeacero />
+          </a>
+          <h2><b>PLATAFORMA</b><br/>CALCULADORA DE ACERO</h2>
+        </div>
+        <div className="item">
+          <a href="https://www.auto-construccion.com" target="_blank">
+            <Logoautoconstruccion />
+          </a>
+          <h2><b>PLATAFORMA</b><br/>AUTOCONSTRUCCIÓN</h2>
+        </div>
       </div>
     </div>
   );
