92a5f3a5ec7e0a18392688bd72166eb263bef794
Axel <axosar@gmail.com>
2019-10-14 11:38:14 -0500

    Adding some styles

--- src/components/Introduccion.js
+++ src/components/Introduccion.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Logoimcycblanco from '../logo-imcyc-blanco.svg';
 import { ReactComponent as Prensa } from '../images/prensa.svg';
 import { ReactComponent as Planta } from '../images/plantas.svg';
 import { ReactComponent as Laboratorio } from '../images/laboratorio.svg';
@@ -9,14 +8,14 @@ import { ReactComponent as Revolvedora } from '../images/revolvedora.svg';
 import { ReactComponent as Logoredcyc } from '../images/logo-redcyc.svg';
 import { ReactComponent as Logoautoconstruccion } from '../images/logo-autoconstruccion.svg';
 import { ReactComponent as Logodeacero } from '../images/logo-deacero.svg';
-import Navbar from './Navbar';
+import { ReactComponent as Bolsa } from '../images/bolsa.svg';
+
 import './Introduccion.css';
 
 function Introduccion() {
   return (
     <div className="introduccion">
-      <Navbar/>
-      
+      <img src={Logoimcycblanco} className="logo" alt="logo" />
       <Link to="/"><h1>APLICACIONES PARA LA CONSTRUCCIÓN</h1></Link>
       <h2>INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h2>
       <div className="container">
@@ -24,25 +23,29 @@ function Introduccion() {
           <a href="http://18.220.192.140:8080/" target="_blank">
             <Revolvedora />
           </a>
-          <h2><b>CERTIFICACIÓN</b><br/>REVOLVEDORA DE CONCRETO</h2>
+          <h2>CERTIFICACIÓN</h2>
+          <h3>REVOLVEDORAS DE CONCRETO</h3>
         </div>
         <div className="item">
           <a href="http://www.redcyc.com/plantasdeconcreto/" target="_blank">
             <Planta />
           </a>
-          <h2><b>CERTIFICACIÓN</b><br/>PLANTAS DE CONCRETO</h2>
+          <h2>CERTIFICACIÓN</h2>
+          <h3>PLANTAS DE CONCRETO</h3>
         </div>
         <div className="item">
           <a href="http://18.216.111.243/" target="_blank">
             <Prensa />
           </a>
-          <h2><b>CERTIFICACIÓN</b><br/>PRENSAS DE CONCRETO</h2>
+          <h2>CERTIFICACIÓN</h2>
+          <h3>PRENSAS DE CONCRETO</h3>
         </div>
         <div className="item">
           <a href="http://18.188.251.124/" target="_blank">
             <Laboratorio />
           </a>
-          <h2><b>CERTIFICACIÓN</b><br/>LABORATORIO</h2>
+          <h2>CERTIFICACIÓN</h2>
+          <h3>LABORATORIOS DE CONCRETO</h3>
         </div>
       </div>
       <div className="container">
@@ -50,25 +53,29 @@ function Introduccion() {
           <a href="http://www.redcyc.com/" target="_blank">
             <Logoredcyc />
           </a>
-          <h2><b>PLATAFORMA</b><br/>REDCYC</h2>
+          <h2>PLATAFORMA</h2>
+          <h3>REDCYC</h3>
         </div>
         <div className="item">
           <a href="https://www.auto-construccion.com" target="_blank">
             <Logoautoconstruccion />
           </a>
-          <h2><b>PLATAFORMA</b><br/>AUTOCONSTRUCCIÓN</h2>
+          <h2>PLATAFORMA</h2>
+          <h3>AUTOCONSTRUCCIÓN</h3>
         </div>
         <div className="item">
           <a href="http://imcyc.net/acero/" target="_blank">
             <Logodeacero />
           </a>
-          <h2><b>PLATAFORMA</b><br/>CALCULADORA DE ACERO</h2>
+          <h2>PLATAFORMA</h2>
+          <h3>CALCULADORA DE ACERO</h3>
         </div>
         <div className="item">
           <a href="https://www.auto-construccion.com" target="_blank">
-            <Logoautoconstruccion />
+            <Bolsa />
           </a>
-          <h2><b>PLATAFORMA</b><br/>AUTOCONSTRUCCIÓN</h2>
+          <h2>PLATAFORMA</h2>
+          <h3>BOLSA DE TRABAJO</h3>
         </div>
       </div>
     </div>
